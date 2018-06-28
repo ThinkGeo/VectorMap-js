93c102584169eab6cad34fba09e5badefada8dbe
Emil-ThinkGeo <emilzhou@thinkgeo.com>
2018-06-28 15:40:00 +0800

    Update build tool

--- build/build.bat
+++ build/build.bat
@@ -1,18 +1,17 @@
 @echo off
-cd %~pd0 
 cd ..
 call npm install 
 call npm install uglify-js -g 
 call npm run build 
-call copy style  "dist" 
-move dist\ol.mapsuite.js  "build\compression" 
-copy src\ol  "build\compression"   
-cd %~pd0build\compression 
+call copy style  "dist"
+move dist\ol.mapsuite.js  %~pd0compression
+copy src\ol  %~pd0compression   
+cd %~pd0compression 
 call npm link 
 call merge.bat  
-call move mapsuite-vectormap-dev.js  %~pd0dist   
+call move mapsuite-vectormap-dev.js  ../../dist   
 call compression.bat 
-call move mapsuite-vectormap.js  %~pd0dist 
+call move mapsuite-vectormap.js  ../../dist 
 del /q /s  *^ol*
 
 
