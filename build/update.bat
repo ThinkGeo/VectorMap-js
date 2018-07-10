7864a089f13e673cf7632343ad123031351afe73
steveliu <steveliu@thinkgeo.com>
2018-07-10 16:49:00 +0800

    update .bat file

--- build/update.bat
+++ build/update.bat
@@ -9,11 +9,16 @@ set newversion=1
 (for /f "tokens=* delims= " %%i in (%config%) do (
     set s=%%i
     if "!s:~12,7!" neq "%commitid%" (
-        set  "cur=!s:~15,1!"
-        set /a cur=cur+1
+        set  "cur=!s:~21,3!"
+        set /a cur=1!cur!-1000+1
+		if !cur! LEQ 9 (
+			set cur=00!cur!
+		)
+		if !cur! GTR 9 if !cur! LEQ 99 (
+			set cur=0!cur!)
         if "!s:~1,7!" == "version" (
-            set "newversion=!s:~11,4!!cur!"
-                echo "version":"!s:~11,4!!cur!")
+            set "newversion=!s:~11,10!!cur!"
+                echo "version":"!s:~11,10!!cur!")
         if "!s:~1,8!" == "lastHead" (
             echo "lastHead":"%commitid%",)
         if "!s:~1,7!" neq "version" if "!s:~1,8!" neq "lastHead" (echo %%i)
