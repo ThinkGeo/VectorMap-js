64107aaa5d63778d0991e706a6a926357238fe98
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2018-11-29 17:46:44 +0800

    add vector tile gray style map
    Signed-off-by: Katie-ThinkGeo <katiezhang@thinkgeo.com>

--- example/js/vector-tile.js
+++ example/js/vector-tile.js
@@ -1,43 +1,19 @@
 WebFont.load({
     custom: {
-        families: ['vectormap-icons'],
-        urls: ['https://cdn.thinkgeo.com/vectormap-icons/1.0.0/vectormap-icons.css']
+        families: ["vectormap-icons"],
+        urls: ['https://cdn.thinkgeo.com/vectormap-icons/2.0.0-beta004/vectormap-icons.css']
     }
 });
 
+var layer = new ol.mapsuite.VectorTileLayer('../data/vectortils_gray.json');
 
-const styleJson = {
-    light: 'http://cdn.thinkgeo.com/worldstreets-styles/1.0.0/light.json',
-}
-const apiKey = 'v8pUXjjVgVSaUOhJCZENyNpdtN7_QnOooGkG0JxEdcI~'
-
-let light = new ol.mapsuite.VectorTileLayer(styleJson.light, {
-    apiKey: apiKey,
-    layerName: 'light'
-});
-
-let map = new ol.Map({
-    layers: [light],
+var map = new ol.Map({
+    layers: [layer],
     target: 'map',
     view: new ol.View({
-        center: ol.proj.fromLonLat([-96.79620, 32.79423]),
-        zoom: 16,
-        minZoom: 2
-    })
+        center: ol.proj.fromLonLat([-98.413148, 38.736301]),
+        zoom: 5,
+        minZoom: 2,
+        maxZoom: 19
+    }),
 });
-
-map.on('pointermove', showInfo);
-
-var info = document.getElementById('info');
-function showInfo(event) {
-    var features = map.getFeaturesAtPixel(event.pixel);
-    if (!features) {
-        info.innerText = '';
-        info.style.opacity = 0;
-        return;
-    }
-    var properties = features[0].getProperties();
-    info.innerText = JSON.stringify(properties, null, 2);
-    info.style.opacity = 1;
-}
-
