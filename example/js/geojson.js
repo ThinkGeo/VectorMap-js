81350b62618d4ba99ff5672843b1464d589a1112
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2018-11-30 17:14:57 +0800

    geojson: modify geojson map theme

--- example/js/geojson.js
+++ example/js/geojson.js
@@ -1,48 +1,47 @@
-const geosjonStyle =
-{
+const geosjonStyle = {
     "id": "thinkgeo-world-streets-light",
     "version": 1.3,
     "owner": "ThinkGeo LLC",
     "time": "2018/06/09",
     "background": "#aac6ee",
-    "variables": {
-    },
+    "variables": {},
     "styles": [{
-        "id": "country",
-        "style": [{
-            "filter": "zoom>=0;zoom<=3;",
-            "polygon-fill": "#b3b9f5"
+            "id": "country",
+            "style": [{
+                "filter": "zoom>=0;zoom<=22;",
+                "polygon-fill": "#64748e"
+            }]
         },
         {
-            "filter": "zoom>=4;zoom<=22;",
-            "polygon-fill": "#6066a9"
-        }]
-    },
-    {
-        "id": "country_boundary",
-        "style": [{
-            "filter": "zoom>=0;zoom<=3;",
-            "line-width": 3,
-            "line-color": "rgba(0, 0, 0, 0.4)",
-        },
-        {
-            "filter": "zoom>=4;zoom<=22;",
-            "line-width": 3,
-            "line-color": "rgba(0, 0, 0, 0.6)",
-        }]
-    }, {
-        "id": "country_name",
-        "style": [{
-            "text-name": "name",
-            "text-wrap-width": 20,
-            "text-fill": "#496588",
-            "text-halo-fill": "rgba(255, 255, 255, 0.5)",
-            "text-halo-radius": 2,
-            "text-font": "oblique 600 16px Arial, Helvetica, sans-serif"
-
+            "id": "country_boundary",
+            "style": [{
+                    "filter": "zoom>=0;zoom<=3;",
+                    "line-width": 2,
+                    "line-color": "rgba(255, 255, 255, 0.4)",
+                },
+                {
+                    "filter": "zoom>=4;zoom<=22;",
+                    "line-width": 3,
+                    "line-color": "rgba(255, 255, 255, 0.6)",
+                }
+            ]
+        }, {
+            "id": "country_name",
+            "style": [{
+                "text-name": "name",
+                "text-wrap-width": 20,
+                "text-fill": "#496588",
+                "text-halo-fill": "rgba(255, 255, 255, 0.5)",
+                "text-halo-radius": 2,
+                "style": [
+                    {
+                        "filter": "zoom>=3;zoom<=22;",
+                        "text-font": "oblique 600 16px Arial, Helvetica, sans-serif",
+                    }
+                ]
+            }]
         }
-        ]
-    }],
+    ],
     "sources": [{
         "id": "countries_source",
         "url": "../data/countries.json",
@@ -64,9 +63,9 @@ let geoVectorLayer = new ol.mapsuite.VectorLayer(geosjonStyle, {
 })
 
 let view = new ol.View({
-    center: ol.proj.fromLonLat([-96.79620, 32.79423]),
-    zoom: 3,
-    minZoom: 2,
+    center: ol.proj.fromLonLat([-10.79620, 32.79423]),
+    zoom: 1,
+    minZoom: 1,
     maxZoom: 19,
     maxResolution: 40075016.68557849 / 512
 });
