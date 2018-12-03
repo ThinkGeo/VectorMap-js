13e97a435d75667a688bfbed02652b34072878f1
JudyZhu <zhubingyu.ice@gmail.com>
2018-12-03 08:25:09 +0800

    on prohrass

--- example/js/labeling-features.js
+++ example/js/labeling-features.js
@@ -9,73 +9,128 @@ WebFont.load({
 const worldstreetsStyle = "https://cdn.thinkgeo.com/worldstreets-styles/1.0.0/light.json";
 
 //block  map style 
-const blockMapStyle = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: '#afaeb1'
-    }),
-    stroke: new ol.style.Stroke({
-        color: '#a59f80',
-        width: 2
-    }),
-    text: new ol.style.Text({
-        font: '14px Calibri,sans-serif',
-        fill: new ol.style.Fill({
-            color: '#525255'
-        }),
-        placement: 'line',
-        stroke: new ol.style.Stroke({
-            color: '#fff',
-            width: 3
-        }),
-    })
-})
+// const blockMapStyle = new ol.style.Style({
+//     fill: new ol.style.Fill({
+//         color: '#afaeb1'
+//     }),
+//     stroke: new ol.style.Stroke({
+//         color: '#a59f80',
+//         width: 2
+//     }),
+//     text: new ol.style.Text({
+//         font: '14px Calibri,sans-serif',
+//         fill: new ol.style.Fill({
+//             color: '#525255'
+//         }),
+//         placement: 'line',
+//         stroke: new ol.style.Stroke({
+//             color: '#fff',
+//             width: 3
+//         }),
+//     })
+// })
 
-const pointStyle = new ol.style.Style({
-    image: new ol.style.Icon({
-        anchor: [1, 1],
-        offset: [0, 3],
-        src: '../image/hotel.png',
-    }),
-    text: new ol.style.Text({
-        font: '12px Calibri,sans-serif',
-        fill: new ol.style.Fill({
-            color: '#ff3467'
-        }),
-        stroke: new ol.style.Stroke({
-            color: '#fff',
-            width: 3
-        }),
+// const pointStyle = new ol.style.Style({
+//     image: new ol.style.Icon({
+//         anchor: [1, 1],
+//         offset: [0, 3],
+//         src: '../image/hotel.png',
+//     }),
+//     text: new ol.style.Text({
+//         font: '12px Calibri,sans-serif',
+//         fill: new ol.style.Fill({
+//             color: '#ff3467'
+//         }),
+//         stroke: new ol.style.Stroke({
+//             color: '#fff',
+//             width: 3
+//         }),
      
-    })
+//     })
+// })
+
+// //point  layer
+// let pointLayer = new ol.layer.Vector({
+//     source: new ol.source.Vector({
+//         url: '../data/hotels.json',
+//         format: new ol.format.GeoJSON()
+//     }),
+//     style: function (feature) {
+//         pointStyle.getText().setText(feature.get('NAME'));
+//         return pointStyle;
+//     }
+// });
+
+
+// let blockMapLayer = new ol.layer.Vector({
+//     source: new ol.source.Vector({
+//         url: '../data/label.json',
+//         format: new ol.format.GeoJSON()
+//     }),
+//     style: function (feature) {
+//         blockMapStyle.getText().setText(feature.get('NAME'));
+//         return blockMapStyle;
+//     }
+// });
+
+const geosjonStyle = {
+    "id": "thinkgeo-world-streets-light",
+    "version": 1.3,
+    "owner": "ThinkGeo LLC",
+    "time": "2018/06/09",
+    "background": "#aac6ee",
+    "variables": {},
+    "styles": [
+        {
+            "id": "block_boundary",
+            "style": [{
+                    "filter": "zoom>=0;zoom<=22;",
+                    "line-width": 2,
+                    "line-color": "a59f80",
+                }
+            ]
+        }, {
+            "id": "block_name",
+            "style": [{
+                "text-name": "NAME",
+              
+                "text-fill": "#496588",
+                "text-halo-fill": "rgba(255, 255, 255, 0.5)",
+                "text-halo-radius": 2,
+                "text-force-horizontal-for-line": true,
+                "style": [
+                    {
+                        "filter": "zoom>=3;zoom<=22;",
+                        "text-font": "oblique 600 16px Arial, Helvetica, sans-serif",
+                    }
+                ]
+            }]
+        }
+    ],
+    "sources": [{
+        "id": "block_source",
+        "url": "../data/label.json",
+        "type": "GeoJSON",
+        "dataProjection":"EPSG:3857",
+        "featureProjection":"EPSG:4326"
+    }],
+    "layers": [{
+        "id": "worldstreets_layers",
+        "source": "block_source",
+        "styles": [
+            "block_boundary", "block_name"
+        ]
+    }]
+}
+
+let blockMapLayer = new ol.mapsuite.VectorLayer(geosjonStyle, {
+    multithread: false
 })
 
-//point  layer
-let pointLayer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '../data/hotels.json',
-        format: new ol.format.GeoJSON()
-    }),
-    style: function (feature) {
-        pointStyle.getText().setText(feature.get('NAME'));
-        return pointStyle;
-    }
-});
-
-
-let blockMapLayer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '../data/label.json',
-        format: new ol.format.GeoJSON()
-    }),
-    style: function (feature) {
-        blockMapStyle.getText().setText(feature.get('NAME'));
-        return blockMapStyle;
-    }
-});
 
 
 let map = new ol.Map({
-    layers: [blockMapLayer, pointLayer],
+    layers: [blockMapLayer],
     target: 'map',
     view: new ol.View({
         center: ol.proj.fromLonLat([-96.820787, 33.098294]),
--- example/js/styling-points.js
+++ example/js/styling-points.js
@@ -13,27 +13,75 @@ const worldstreets = new ol.mapsuite.VectorTileLayer(worldstreetsStyle,
         apiKey: 'v8pUXjjVgVSaUOhJCZENyNpdtN7_QnOooGkG0JxEdcI~'      // please go to https://cloud.thinkgeo.com to create
     });
 
-let pointLayer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        url: '../data/Frisco-school-poi.json',
-        format: new ol.format.GeoJSON()
-    }),
-    style: new ol.style.Style({
-        image: new ol.style.Icon({
-            anchor: [0, 0],
-            anchorXUnits: 'fraction',
-            anchorYUnits: 'pixels',
-            src: '../image/school.png'
-        })
-    })
-});
+
+const geosjonStyle = {
+    "id": "Frisco-school-poi",
+    "version": 1.3,
+    "owner": "ThinkGeo LLC",
+    "background": "#aac6ee",
+    "variables": {},
+    "styles": [{
+        "id": "poi_icon",
+        "point-type": "glyph",
+        "point-glyph": "vectormap-icons",
+        "point-fill": "#439c3c",
+        "point-size": 22,
+        "line-width": 3,
+        "line-color": "rgba(255, 255, 255, 0.6)",
+        // "point-glyph-name": "\e0a8",
+        "point-outline-color": "#ffffff",
+        "point-outline-width": 3,
+        // "style": [{
+        //     "style": [{
+        //         "point-fill": "5d869b",
+        //         "point-glyph-name": "\e0a8"
+        //     }]
+        // }]
+           
+        },
+        {
+            "id": "country_name",
+            "style": [{
+                "text-name": "NAME",
+                "text-wrap-width": 20,
+                "text-fill": "#496588",
+                "text-halo-fill": "rgba(255, 255, 255, 0.5)",
+                "text-halo-radius": 2,
+                "style": [
+                    {
+                        "filter": "zoom>=3;zoom<=22;",
+                        "text-font": "oblique 600 16px Arial, Helvetica, sans-serif",
+                    }
+                ]
+            }]
+        }
+    ],
+    "sources": [{
+        "id": "school_source",
+        "url": "../data/Frisco-school-poi.geojson",
+        "type": "GeoJSON",
+        "dataProjection":"EPSG:3857",
+        "featureProjection":"EPSG:4326"
+    }],
+    "layers": [{
+        "id": "worldstreets_layers",
+        "source": "school_source",
+        "styles": [
+            "poi_icon","country_name"
+        ]
+    }]
+}
+
+let pointLayer = new ol.mapsuite.VectorLayer(geosjonStyle, {
+    multithread: false
+})
 
 let map = new ol.Map({
-    layers: [worldstreets, pointLayer],
+    layers: [pointLayer],
     target: 'map',
     view: new ol.View({
         center: ol.proj.fromLonLat([-96.79620, 33.15423]),
-        zoom: 12,
+        zoom: 14,
     }),
 });
 
