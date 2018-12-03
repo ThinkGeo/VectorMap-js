1704fdf1099b876797128fb28f48bf987488bfab
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2018-12-03 13:52:20 +0800

    Styling Lines: Dletet webfont loader

--- example/js/styling-lines.js
+++ example/js/styling-lines.js
@@ -1,10 +1,3 @@
-WebFont.load({
-    custom: {
-        families: ["vectormap-icons"],
-        urls: ["https://cdn.thinkgeo.com/vectormap-icons/1.0.0/vectormap-icons.css"]
-    }
-});
-
 const baseMapStyle = {
     "id": "europe",
     "version": 1.3,
