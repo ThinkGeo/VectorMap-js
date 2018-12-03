3fb8339655ba6b6b2e07c27f706f858ac76191a0
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2018-12-03 14:03:07 +0800

    Labeling Features:  "text-force-horizontal-for-line": false,

--- example/js/labeling-features.js
+++ example/js/labeling-features.js
@@ -8,71 +8,6 @@ WebFont.load({
 
 const worldstreetsStyle = "https://cdn.thinkgeo.com/worldstreets-styles/1.0.0/light.json";
 
-//block  map style 
-// const blockMapStyle = new ol.style.Style({
-//     fill: new ol.style.Fill({
-//         color: '#afaeb1'
-//     }),
-//     stroke: new ol.style.Stroke({
-//         color: '#a59f80',
-//         width: 2
-//     }),
-//     text: new ol.style.Text({
-//         font: '14px Calibri,sans-serif',
-//         fill: new ol.style.Fill({
-//             color: '#525255'
-//         }),
-//         placement: 'line',
-//         stroke: new ol.style.Stroke({
-//             color: '#fff',
-//             width: 3
-//         }),
-//     })
-// })
-
-// const pointStyle = new ol.style.Style({
-//     image: new ol.style.Icon({
-//         anchor: [1, 1],
-//         offset: [0, 3],
-//         src: '../image/hotel.png',
-//     }),
-//     text: new ol.style.Text({
-//         font: '12px Calibri,sans-serif',
-//         fill: new ol.style.Fill({
-//             color: '#ff3467'
-//         }),
-//         stroke: new ol.style.Stroke({
-//             color: '#fff',
-//             width: 3
-//         }),
-     
-//     })
-// })
-
-// //point  layer
-// let pointLayer = new ol.layer.Vector({
-//     source: new ol.source.Vector({
-//         url: '../data/hotels.json',
-//         format: new ol.format.GeoJSON()
-//     }),
-//     style: function (feature) {
-//         pointStyle.getText().setText(feature.get('NAME'));
-//         return pointStyle;
-//     }
-// });
-
-
-// let blockMapLayer = new ol.layer.Vector({
-//     source: new ol.source.Vector({
-//         url: '../data/label.json',
-//         format: new ol.format.GeoJSON()
-//     }),
-//     style: function (feature) {
-//         blockMapStyle.getText().setText(feature.get('NAME'));
-//         return blockMapStyle;
-//     }
-// });
-
 const geosjonStyle = {
     "id": "thinkgeo-world-streets-light",
     "version": 1.3,
@@ -84,7 +19,7 @@ const geosjonStyle = {
         {
             "id": "block_boundary",
             "style": [{
-                    "filter": "zoom>=0;zoom<=22;",
+                    "filter": "zoom>=0;zoom<=19;",
                     "line-width": 2,
                     "line-color": "a59f80",
                 }
@@ -97,10 +32,10 @@ const geosjonStyle = {
                 "text-fill": "#496588",
                 "text-halo-fill": "rgba(255, 255, 255, 0.5)",
                 "text-halo-radius": 2,
-                "text-force-horizontal-for-line": true,
+                "text-force-horizontal-for-line": false,
                 "style": [
                     {
-                        "filter": "zoom>=3;zoom<=22;",
+                        "filter": "zoom>=3;zoom<=19;",
                         "text-font": "oblique 600 16px Arial, Helvetica, sans-serif",
                     }
                 ]
@@ -127,14 +62,14 @@ let blockMapLayer = new ol.mapsuite.VectorLayer(geosjonStyle, {
     multithread: false
 })
 
-
-
 let map = new ol.Map({
     layers: [blockMapLayer],
     target: 'map',
     view: new ol.View({
         center: ol.proj.fromLonLat([-96.820787, 33.098294]),
         zoom: 17,
+        minZoom:0,
+        maxZoom:19
     }),
 });
 
