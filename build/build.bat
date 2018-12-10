4cd47cab567502964db07f0cd1ae7af87b22b484
Emil Zhou <emilzhou@thinkgeo.com>
2018-12-10 15:22:37 +0800

    Update build.bat for new relese

--- build/build.bat
+++ build/build.bat
@@ -5,15 +5,7 @@ call npm install uglify-js -g
 call npm run build 
 call copy style  "dist"
 call copy README.md  "dist"
-move dist\ol.mapsuite.js  %~pd0compression
-copy src\ol  %~pd0compression   
-cd %~pd0compression 
-call npm link 
-call merge.bat  
-call move vectormap-dev.js  ../../dist   
-call compression.bat 
-call move vectormap.js  ../../dist 
-del /q /s  *^ol*
+
 
 
 
