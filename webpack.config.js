e4085cdfaa69126a743758f60362a032b389f82e
Emil Zhou <emilzhou@thinkgeo.com>
2018-12-10 15:24:41 +0800

    Update the package of project and the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,59 @@
-const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/main.ts',
-     devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'ol.mapsuite.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+
+var developConfig = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'vectormap-dev.js',
+    library: 'ol',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.worker\.js$/,
+        use: {
+          loader: 'worker-loader',
+          options: { inline: true, fallback: false }
+        }
+      }
+    ]
+  },
+  devServer: {
+    openPage: "./debug",
+    host: 'localhost',
+    compress: true,
+    port: 8080
+  },
 };
+
+var releaseConfig= {
+  entry: './src/index.js',
+  output: {
+    filename: 'vectormap.js',
+    library: 'ol',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.worker\.js$/,
+        use: {
+          loader: 'worker-loader',
+          options: { inline: true, fallback: false }
+        }
+      }
+    ]
+  },
+  devServer: {
+    openPage: "./debug",
+    host: 'localhost',
+    compress: true,
+    port: 8080
+  },
+};
+module.exports = [developConfig,releaseConfig];
