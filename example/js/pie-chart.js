55a18659ccd9cdd95c51c662e992363b8fd134e9
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2018-12-20 19:25:09 +0800

    add new education data and change the data of pie-chart

--- example/js/pie-chart.js
+++ example/js/pie-chart.js
@@ -1,6 +1,4 @@
 // base map layer
-
-
 let satelliteLayer = new ol.layer.Tile({
     source: new ol.source.XYZ({
         url: "https://cloud.thinkgeo.com/api/v1/maps/raster/dark/x1/3857/512/{z}/{x}/{y}.png" +
@@ -18,7 +16,9 @@ let map = new ol.Map({
     target: 'map',
     view: new ol.View({
         center: ol.proj.fromLonLat([-95.940014, 36.031607]),
-        maxZoom: 19,maxResolution: 40075016.68557849 / 512,zoom: 4
+        maxZoom: 19,
+        maxResolution: 40075016.68557849 / 512,
+        zoom: 4
     })
 });
 
@@ -53,226 +53,105 @@ let pieChartOvery = (id, data, pt) => {
     });
 }
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 26; i++) {
     let aqiDiv = document.createElement("div");
     aqiDiv.id = `pieChart${i}`;
     aqiDiv.style = "height:22vh;width:600px";
     document.querySelector("#pieChart").appendChild(aqiDiv)
 }
 
-//simulated data 
-let California = [{
-    value: 4572963,
-    name: `Under high  school `,
-    itemStyle: {
-        color: '#fe0100',
-    }
-}, {
-    value: 5260904,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 7544058,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 8176487,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}];
-let Florida = [{
-    value: 1807386,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
-    }
-}, {
-    value: 4111029,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 0245476,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 3929796,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}];
-let Colorado = [{
-    value: 323691,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
-    }
-}, {
-    value: 780033,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 1096896,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 1389730,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}];
+const getJson = () => {
+    let readTextFile = new Promise(function (resolve, reject) {
+        let file = "../data/education.geojson";
+        let rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function (ERR) {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status == "200") {
+                    resolve(rawFile.responseText);
+                } else {
+                    reject(new Error(ERR));
+                }
+            }
+        }
+        rawFile.send(null);
+    });
+    return readTextFile;
+}
 
-let Illinois = [{
-    value: 1008608,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
-    }
-}, {
-    value: 2287126,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 2487681,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 2834689,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}]
+let stateArr = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
 
-let Georgia = [{
-    value: 932810,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
-    }
-}, {
-    value: 1850601,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 1867961,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 1938090,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}]
+getJson().then(function (data) {
+    let resultFeatures = JSON.parse(data)['features'];
+    for (let i = 0, l = resultFeatures.length; i < l; i++) {
+        let item = resultFeatures[i]['properties'];
+        let coor = resultFeatures[i]['geometry']['coordinates'];
+        let bcav = item.Bcav;
+        let Col = item.Col;
+        let HS = item.HS;
+        let UHSC = item.UHSC;
 
-let Texas = [{
-    value: 1008608,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
-    }
-}, {
-    value: 2287126,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 24873681,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 2834869,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}]
+        stateArr[i].push({
+            value: UHSC,
+            name: `Under high school `,
+            itemStyle: {
+                color: '#fe0100',
+            }
+        });
+        stateArr[i].push({
+            value: HS,
+            name: 'High school ',
+            itemStyle: {
+                color: '#f9ff00'
+            }
+        });
+        stateArr[i].push({
+            value: Col,
+            name: `College`,
+            itemStyle: {
+                color: '#a4e601'
+            }
+        });
+        stateArr[i].push({
+            value: bcav,
+            name: `Bachelor degree and above`,
+            itemStyle: {
+                color: '#1e86fe'
+            }
+        });
 
-let Dakota = [{
-    value: 38034,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
+        let index = `pieChart${i}`;
+        console.log(index);
+        console.log(stateArr[i]);
+        console.log(ol.proj.fromLonLat(coor));
+        map.addOverlay(pieChartOvery(index, stateArr[i], ol.proj.fromLonLat(coor)));
     }
-}, {
-    value: 131086,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 173933,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 134554,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}]
-
-let Mexico = [{
-    value: 517458,
-    name: 'Under high  school ',
-    itemStyle: {
-        color: '#fe0100'
-    }
-}, {
-    value: 1486051,
-    name: 'High school ',
-    itemStyle: {
-        color: '#f9ff00'
-    }
-}, {
-    value: 1268116,
-    name: `College or associate`,
-    itemStyle: {
-        color: '#a4e601'
-    }
-}, {
-    value: 1068062,
-    name: `Bachelor degree and above`,
-    itemStyle: {
-        color: '#1e86fe'
-    }
-}]
-
-map.addOverlay(pieChartOvery("pieChart0", California, ol.proj.fromLonLat([-119.23484, 35.675786])));
-map.addOverlay(pieChartOvery("pieChart1", Florida, ol.proj.fromLonLat([-81.956835, 27.985804])))
-map.addOverlay(pieChartOvery("pieChart2", Colorado, ol.proj.fromLonLat([-104.940303, 39.885961])))
-map.addOverlay(pieChartOvery("pieChart3", Illinois, ol.proj.fromLonLat([-88.132874, 39.683286])))
-map.addOverlay(pieChartOvery("pieChart4", Georgia, ol.proj.fromLonLat([-84.754272, 34.126149])))
-map.addOverlay(pieChartOvery("pieChart5", Dakota, ol.proj.fromLonLat([-100.144579, 46.843928])))
-map.addOverlay(pieChartOvery("pieChart6", Texas, ol.proj.fromLonLat([-97.75, 30.266667])))
-map.addOverlay(pieChartOvery("pieChart7", Mexico, ol.proj.fromLonLat([-105.22421, 35.58103])))
-
+})
