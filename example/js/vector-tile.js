116ac5d5523b7f4f7549b4da9a3fbfab18ef7c9d
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2019-02-27 15:50:55 +0800

    Added full expository comments to the Style ThinkGeo Maps sample JS and HTML.

--- example/js/vector-tile.js
+++ example/js/vector-tile.js
@@ -1,5 +1,35 @@
- 
-//Load icon font
+ /*===========================================================================*/
+// Style ThinkGeo Maps
+// Sample map by ThinkGeo
+// 
+//   1. ThinkGeo Cloud API Key
+//   2. ThinkGeo Map Icon Fonts
+//   3. Map Control Setup
+//   4. Updating Style Setup
+//   5. Event Listeners
+/*===========================================================================*/
+
+
+/*---------------------------------------------*/
+// 1. ThinkGeo Cloud API Key
+/*---------------------------------------------*/
+
+// First, let's define our ThinkGeo Cloud API key, which we'll use to
+// authenticate our requests to the ThinkGeo Cloud API.  Each API key can be
+// restricted for use only from a given web domain or IP address.  To create your
+// own API key, you'll need to sign up for a ThinkGeo Cloud account at
+// https://cloud.thinkgeo.com.
+const apiKey = 'WPLmkj3P39OPectosnM1jRgDixwlti71l8KYxyfP2P0~'
+
+
+/*---------------------------------------------*/
+// 2. ThinkGeo Map Icon Fonts
+/*---------------------------------------------*/
+
+// Now we'll load the Map Icon Fonts using the WebFont loader. The loaded 
+// Icon Fonts will be rendered as POI icons on the background layer. 
+// For more info, see our wiki: 
+// https://wiki.thinkgeo.com/wiki/thinkgeo_iconfonts 
 WebFont.load({
     custom: {
         families: ["vectormap-icons"],
@@ -7,30 +37,50 @@ WebFont.load({
     }
 });
 
-//create default layer
+
+/*---------------------------------------------*/
+// 3. Map Control Setup
+/*---------------------------------------------*/
+
+// Now we'll create the layer for our map. The layer uses the ThinkGeo Cloud 
+// Maps Vector Tile service to display a detailed map.  For more info, see our wiki:
+// https://wiki.thinkgeo.com/wiki/thinkgeo_cloud_maps_vector_tiles 
 let layer = new ol.mapsuite.VectorTileLayer('../data/vectortils_gray.json', {
-    'apiKey': 'WPLmkj3P39OPectosnM1jRgDixwlti71l8KYxyfP2P0~'
+    'apiKey': apiKey
 });
 
-//create map
+// Create and initialize our map control.
 let map = new ol.Map({
     loadTilesWhileAnimating: true,
     loadTilesWhileInteracting: true,
+    // Add our previously-defined ThinkGeo Cloud Vector Tile layer to the map.
     layers: [layer],
+    // States that the HTML tag with id="map" should serve as the container for our map.
     target: 'map',
+    // Create a default view for the map when it starts up.
     view: new ol.View({
+        // Center the map on the Washington and start at zoom level 15.
         center: ol.proj.fromLonLat([-77.043745, 38.895620]),
-        maxZoom: 19,
         maxResolution: 40075016.68557849 / 512,
         zoom: 15,
         minZoom: 2,
+        maxZoom: 19
     }),
 });
 
-//Full screen interaction
+// Add a button to the map that lets us toggle full-screen display mode.
 map.addControl(new ol.control.FullScreen());
 
-//load geojson file
+
+/*---------------------------------------------*/
+// 4. Updating Style Setup
+/*---------------------------------------------*/
+
+// Next, we have to get one copy of the StyleJSON file data. Then we can modify some 
+// styles to create a new one. Then we could apply the new StyleJSON to a new 
+// style layer.
+
+// Read a StyleJSON file data and assign the value to the variable -- stylejson.
 let getJson = () => {
     let readTextFile = new Promise(function (resolve, reject) {
         let file = "../data/vectortils_gray.json";
@@ -51,13 +101,13 @@ let getJson = () => {
     return readTextFile;
 }
 
-//Parses a JSON string
+// Parse the JSON string, constructing the JavaScript value described by the string.
 let stylejson;
 getJson().then((data) => {
     stylejson = JSON.parse(data);
 })
 
-//Updated style json by user input value
+// This method update the StyleJSON value by what user input.
 const updateStyleJson = (poiSize, waterColor, buildingColor, placement) => {
     let styles = stylejson.styles;
     let stylesLength = styles.length;
@@ -84,16 +134,27 @@ const updateStyleJson = (poiSize, waterColor, buildingColor, placement) => {
             }
         }
     }
-    //Add layer with new style json
+    // Remove the old style layer.
     let layers = map.getLayers().getArray();
     map.removeLayer(layers[0]);
+    // Create a new style layer and add it to the map.
     let newLayer = new ol.mapsuite.VectorTileLayer(stylejson, {
-        'apiKey': 'WPLmkj3P39OPectosnM1jRgDixwlti71l8KYxyfP2P0~'
+        'apiKey': apiKey
     });
     map.addLayer(newLayer);
 }
 
-document.getElementById('generate').addEventListener('click',(generate)=>{
+
+/*---------------------------------------------*/
+// 5. Event Listeners
+/*---------------------------------------------*/
+
+// These event listeners tell the UI when it's time to execute all of the 
+// code we've written.
+
+// This listener will get the values from the input boxes when you click the 
+// "refresh" button, and then uses them to update the map style.
+document.getElementById('refresh').addEventListener('click',(e)=>{
     let userInput={
         poiSize: document.getElementById('poiSize').value,
         waterColor: document.getElementById('water-color').value,
