72d7643b6c540bca3e6a1f8dcf3cf3f2bb3ca5c4
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2019-03-04 16:23:20 +0800

    Added full expository comments to the Global Earthquake Distribution sample JS and HTML.

--- example/js/heatmap-KML.js
+++ example/js/heatmap-KML.js
@@ -1,4 +1,34 @@
-//Load vector map icon font
+/*===========================================================================*/
+// Global Earthquake Distribution
+// Sample map by ThinkGeo
+// 
+//   1. ThinkGeo Cloud API Key
+//   2. ThinkGeo Map Icon Fonts
+//   3. Map Control Setup
+//   4. Display Heat Style Polygons
+/*===========================================================================*/
+
+
+/*---------------------------------------------*/
+// 1. ThinkGeo Cloud API Key
+/*---------------------------------------------*/
+
+// First, let's define our ThinkGeo Cloud API key, which we'll use to
+// authenticate our requests to the ThinkGeo Cloud API.  Each API key can be
+// restricted for use only from a given web domain or IP address.  To create your
+// own API key, you'll need to sign up for a ThinkGeo Cloud account at
+// https://cloud.thinkgeo.com.
+const apiKey = 'WPLmkj3P39OPectosnM1jRgDixwlti71l8KYxyfP2P0~';
+
+
+/*---------------------------------------------*/
+// 2. ThinkGeo Map Icon Fonts
+/*---------------------------------------------*/
+
+// Now we'll load the Map Icon Fonts using the WebFont loader. The loaded 
+// Icon Fonts will be rendered as POI icons on the background layer. 
+// For more info, see our wiki: 
+// https://wiki.thinkgeo.com/wiki/thinkgeo_iconfonts 
 WebFont.load({
     custom: {
         families: ["vectormap-icons"],
@@ -6,50 +36,78 @@ WebFont.load({
     }
 });
 
-const apiKey = 'WPLmkj3P39OPectosnM1jRgDixwlti71l8KYxyfP2P0~';// please go to https://cloud.thinkgeo.com to create
-const worldstreetsStyle = "https://cdn.thinkgeo.com/worldstreets-styles/1.0.0/light.json";
 
-// Base map layer
-let worldStreetLayer = new ol.mapsuite.VectorTileLayer(worldstreetsStyle, {
+/*---------------------------------------------*/
+// 3. Map Control Setup
+/*---------------------------------------------*/
+
+// Now we'll create the base layer for our map.  The base layer uses the ThinkGeo
+// Cloud Maps Vector Tile service to display a detailed street map.  For more
+// info, see our wiki:
+// https://wiki.thinkgeo.com/wiki/thinkgeo_cloud_maps_vector_tiles
+let baseLayer = new ol.mapsuite.VectorTileLayer("https://cdn.thinkgeo.com/worldstreets-styles/1.0.0/light.json", {
     'apiKey': apiKey,
 });
 
-//Heatmap layer
+// Create and initialize our interactive map.
+let map = new ol.Map({
+    loadTilesWhileAnimating: true,
+    loadTilesWhileInteracting: true,
+
+    // Add our previously-defined ThinkGeo Cloud Vector Tile layer to the map.
+    layers: [baseLayer],
+    // States that the HTML tag with id="map" should serve as the container for our map.
+    target: 'map',
+    // Create a default view for the map when it starts up.
+    view: new ol.View({
+        // Center the map on Oceania and start at zoom level 3.
+        center: ol.proj.fromLonLat([149.704275, -15.037667]),
+        maxResolution: 40075016.68557849 / 512,
+        zoom: 3,
+        minZoom: 2,
+        maxZoom: 19
+    })
+});
+
+// Add a button to the map that lets us toggle full-screen display mode.
+map.addControl(new ol.control.FullScreen());
+
+
+/*---------------------------------------------*/
+// 4. Display Heat Style Polygons
+/*---------------------------------------------*/
+
+// This next part sets up the heat style for the placemark on our map. As we 
+// zoom out, the color of the placemarks that are close together will be deepened, 
+// and these placemarks will be merged into one region. This heat map shows magnitude 
+// of each earthquake in each placemark, with color-coded from weakest to strongest.
+
+// Create heat map layer. It uses a xml format data, which is hosted on our server, 
+// to display the heat layer.
 let heatMapLayer = new ol.layer.Heatmap({
     source: new ol.source.Vector({
         url: '../data/2012_Earthquakes_Mag5.xml',
+        // Define the data format as KML.
         format: new ol.format.KML({
             extractStyles: false
         })
     }),
+    // The blur size.
     blur: 15,
+    // The radius size.
     radius: 10
 });
 
-//Add heatmap layer feature
+// When add feature to the heatmap layer, 
 heatMapLayer.getSource().on('addfeature', function (event) {
-    // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
+    // 2012_Earthquakes_Mag5.xml stores the magnitude of each earthquake in a
     // standards-violating <magnitude> tag in each Placemark.  We extract it from
     // the Placemark's name instead.
     let name = event.feature.get('name');
     let magnitude = parseFloat(name.substr(2));
+    // Set weight style for every point according to magnitude.
     event.feature.set('weight', magnitude - 5);
 });
 
-//Create map
-let map = new ol.Map({
-    loadTilesWhileAnimating: true,
-    loadTilesWhileInteracting: true,
-    layers: [worldStreetLayer, heatMapLayer],
-    target: 'map',
-    view: new ol.View({
-        center: ol.proj.fromLonLat([149.704275, -15.037667]),
-        maxZoom: 19,
-        maxResolution: 40075016.68557849 / 512,
-        zoom: 3,
-        minZoom: 2
-    })
-});
-
-//Control map full screen
-map.addControl(new ol.control.FullScreen());
+// Add the pre-defined heatmap layer to our map.
+map.addLayer(heatMapLayer);
