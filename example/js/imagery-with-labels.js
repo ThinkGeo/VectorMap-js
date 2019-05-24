1b840743d83c8a2b6f2926be58a34fefc7218c2c
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2019-05-24 16:46:55 +0800

    Add tile loading error for Vector Maps on Custom Imagery sample.

--- example/js/imagery-with-labels.js
+++ example/js/imagery-with-labels.js
@@ -4,6 +4,7 @@
 // 
 //   1. ThinkGeo Cloud API Key
 //   2. Map Control Setup
+//   3. Tile Loading Event Handlers
 /*===========================================================================*/
 
 
@@ -70,3 +71,31 @@ let map = new ol.Map({
 
 // Add a button to the map that lets us toggle full-screen display mode.
 map.addControl(new ol.control.FullScreen());
+
+
+/*---------------------------------------------*/
+// 3. Tile Loading Event Handlers
+/*---------------------------------------------*/
+
+// These events allow you to perform custom actions when 
+// a map tile encounters an error while loading.
+const errorLoadingTile = () => {
+    const errorModal = document.querySelector('#error-modal');
+    if (errorModal.classList.contains('hide')) {
+        // Show the error tips when Tile loaded error.
+        errorModal.classList.remove('hide');
+    }
+}
+
+const setLayerSourceEventHandlers = (layer) => {
+    let layerSource = layer.getSource();
+    layerSource.on('tileloaderror', function () {
+        errorLoadingTile();
+    });
+}
+
+setLayerSourceEventHandlers(vectorStreetsLayer);
+
+document.querySelector('#error-modal button').addEventListener('click', () => {
+    document.querySelector('#error-modal').classList.add('hide');
+})
