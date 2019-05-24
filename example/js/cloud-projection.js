8ce5468a1b033f94a21f2972c63d5a384b0613ef
Katie-ThinkGeo <katiezhang@thinkgeo.com>
2019-05-24 16:38:45 +0800

    Add tile loading error handler for Transform Projection sample.

--- example/js/cloud-projection.js
+++ example/js/cloud-projection.js
@@ -101,7 +101,7 @@ const performTransform = (wkt) => {
             if(res.data){
                 document.querySelector('.spherical-mercator textarea').value = `${res.status}: ${res.data.wkt}`;
             }else{
-                document.querySelector('.spherical-mercator textarea').value = `${res.status}: ${res.error.message}`;
+                errorLoadingTile();
             }            
         } else {
             document.querySelector('.loading').classList.add('hide');
@@ -131,3 +131,32 @@ WebFont.load({
     },
     active: initializeMap
 });
+
+
+
+/*---------------------------------------------*/
+// Tile Loading Event Handlers
+/*---------------------------------------------*/
+
+// These events allow you to perform custom actions when 
+// a map tile encounters an error while loading.
+const errorLoadingTile = () => {
+    const errorModal = document.querySelector('#error-modal');
+    if (errorModal.classList.contains('hide')) {
+        // Show the error tips when Tile loaded error.
+        errorModal.classList.remove('hide');
+    }
+}
+
+const setLayerSourceEventHandlers = (layer) => {
+    let layerSource = layer.getSource();
+    layerSource.on('tileloaderror', function () {
+        errorLoadingTile();
+    });
+}
+
+setLayerSourceEventHandlers(defaultLayer);
+
+document.querySelector('#error-modal button').addEventListener('click', () => {
+    document.querySelector('#error-modal').classList.add('hide');
+})
