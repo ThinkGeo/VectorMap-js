817c087aa0ac93e2caf8f9c3aae9d2f054bd66a2
Emil Zhou <emilzhou@thinkgeo.com>
2019-12-24 16:38:23 +0800

    Update the sample code

--- debug/index.js
+++ debug/index.js
@@ -1,9 +1,7 @@
+
+
 var view = new ol.View({
-    //center: [0, 0],
-    //center: [-10784844.42768457, 4989250.967286606],// poi
-    // center: [-8231292.426002176, 4952348.061687753],// NY
-    //center:[-8186799.75338646, 4952102.791557407],// NY road label reverse
-    //center: [-10780142.293364197, 3888254.56808908],// dallas road 
+    //center:[-8186799.75338646, 4952102.791557407],zoom:17,// NY road label reverse
     //center: [-10796026.396196617, 5003517.396574807],// country_name
     //center: [-10783010.162497278, 3862161.525031017],// road text 16
     //center :[-10782797.10767367, 3865969.721878732], // road name and one way icon
@@ -11,13 +9,13 @@ var view = new ol.View({
     //center: [-8051563.931156208, 6108477.916978194], // ol polygon 8
     //center:[-15008563.377850933, 4304933.433021126], // country polygon
 
-     //center:[-10781710.244788043, 3888472.76899487],// road clip issue zoom 17.
-    // center:[-10777094.140020758, 3865651.7722501396], 
-    // center:[-10776404.929903472, 3866030.036479002],
+    //center:[-10781710.244788043, 3888472.76899487], zoom:17,// road clip issue zoom 17.
+    //center:[-10774719.96412073, 3866961.8013970205],zoom:17 // footway line dash crash issue.
+    //center: [-10782895.441743676, 3887414.379523998], zoom: 18,// U-turn
 
-    //center:[-10774719.96412073, 3866961.8013970205], // footway line dash crash issue.
-    center: [-25039682.65517518, 4756795.348892094], 
-    zoom: 2,
+    //center: [-22997335.8, -3253574.954307], zoom: 17,// drawing order of road
+    //center: [-10775289.808138244, 3865732.688010553], zoom: 19, //// drawing order of road in Dallas
+    center: [-10781709.991733968, 3888501.660642869], zoom: 19, //// drawing order of road in Dallas
     maxZoom: 19,
     maxResolution: 40075016.68557849 / 512,
     progressiveZoom: true
@@ -113,7 +111,7 @@ var vectorLayer = new ol.layer.Vector({
 })
 vectorLayer.getSource().addFeature(new ol.Feature(new ol.geom.Point([-10783941.374986181, 4990142.424126486])))
 
-var lineStyle= new ol.style.Style({
+var lineStyle = new ol.style.Style({
     stroke: new ol.style.Stroke({
         color: "blue",
         width: 4
@@ -123,23 +121,21 @@ var lineStyle= new ol.style.Style({
 var layer = new ol.layer.VectorTile({
     source: new ol.source.VectorTile({
         format: new ol.format.MVT({
-            featureClass:ol.Feature
+            featureClass: ol.Feature
         }),
         url: 'http://localhost:1314/tile/{z}/{x}/{y}'
     }),
     renderMode: "vector",
-    style:function(f,r)
-    {
-        if(f.get('layer')==='road'&&f.get('class')==='motorway_link')
-        {
+    style: function (f, r) {
+        if (f.get('layer') === 'road' && f.get('class') === 'motorway_link') {
             return lineStyle
         }
     }
 });
 
 var map = new ol.Map({
-     layers: [worldStreetsLayer, vectorLayer,debugLayer],
-     renderer: 'webgl',
+    layers: [worldStreetsLayer, vectorLayer, debugLayer],
+    renderer: 'webgl',
 
     //renderer: ['canvas'],
     //layers: [layer, vectorLayer],
